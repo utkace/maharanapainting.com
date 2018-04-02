abe3054c86613f84e497542a1346a2989176e21a
vaaneet <vaaneetkapoor1970@gmail.com>
2018-04-02 16:38:40 +0530

    menu

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 
 /******* MENU *******/
 $(".first").hover(function() {
-  $(".cover-c").css('background-image', 'url(img/menu/bg1.jpg)')
+  $(".cover-c .sections").css('background-image', 'url(img/menu/bg1.jpg)')
 });
 
 $(".second").hover(function() {
-  $(".cover-c").css('background-image', 'url(img/menu/bg2.jpg)')
+  $(".cover-c .sections").css('background-image', 'url(img/menu/bg2.jpg)')
 });
 
 $(".third").hover(function() {
-  $(".cover-c").css('background-image', 'url(img/menu/bg3.jpg)')
+  $(".cover-c .sections").css('background-image', 'url(img/menu/bg3.jpg)')
 });
 
 $(".fourth").hover(function() {
-  $(".cover-c").css('background-image', 'url(img/menu/bg4.jpg)')
+  $(".cover-c .sections").css('background-image', 'url(img/menu/bg4.jpg)')
 });
 
 $(".fifth").hover(function() {
-  $(".cover-c").css('background-image', 'url(img/menu/bg5.jpg)')
+  $(".cover-c .sections").css('background-image', 'url(img/menu/bg5.jpg)')
 });
 
 /******* INDEX *******/
@@ -30,4 +30,26 @@ $('.cover-c').css('width', '100%');
 
 function dis(){
 $('.cover-c').css('width', '0%');
+};
+
+/*XXXXXXXXXXXXXXXX-ABOUT-XXXXXXXXXXX*/
+var x=screen.width;
+console.log(x);
+function cover() {
+	$('.cover-c').css('width', '100%');
+};
+function dis() {
+	$('.cover-c').css('width', '0%');
+};
+function bot1() {
+	//$('.section-1').css('width', '100%');
+	$('.section-1').fadeToggle(2000); //give width 100%
+};
+function bot2() {
+	//$('.section-2').css('width', '100%');
+	$('.section-2').fadeToggle(2000);
+};
+function bot3() {
+	//$('.section-3').css('width', '100%');
+	$('.section-3').fadeToggle(2000);
 };
